fb969703b16bf9a8e9f62a7c7922004e7576346c
GomezAPiot <kim.gomezjr@student.ap.be>
2021-10-08 10:48:28 +0200

    new adding

---
+++ Labo03/Hello World.py
@@ -0,0 +1,4 @@
+greetings='hello' #dit is de begroeting
+planet=' world' #dit is naar de persoon of object waarnaartoe dit bericht gericht is.
+som=greetings+''+planet #dit is de som die gemaakt word om alle atributen op te sommen.
+print(som)
