909673d54341d090ba0213edf33b971db36a6779
GomezAPiot <kim.gomezjr@student.ap.be>
2021-12-24 09:35:06 +0100

    added turtle screen and defined function draw

--- trainboard/trainboard.py
+++ trainboard/trainboard.py
@@ -1,8 +1,54 @@
-data = {
-    'version': '1.1',
-    'timestamp': '1638823746',
-    'station': [
-        {'id': 'BE.NMBS.000000101', 'name': 'Antwerpen-Centraal'},
-    ]
-}
-print(data['station'][1]['name'])
+import http.client
+import json
+from datetime import datetime
+from turtle import Screen, Turtle
+
+
+con = http.client.HTTPSConnection("api.irail.be")
+con.request(
+    "GET", "/liveboard/?id=BE.NMBS.008892007&arrdep=departure&format=json")
+test1 = con.getresponse()
+print(test1.status)
+test1 = test1.read()
+# print(test1)
+
+dataTest1 = json.loads(test1)
+# print(dataTest1)
+departureList = dataTest1["departures"]["departure"]
+for x in departureList:
+    print(x["station"], datetime.fromtimestamp(int(x["time"])),
+          x["platform"], x["vehicleinfo"]["shortname"])
+    # dest, time, platform, train name
+
+    # print(departureList)
+
+
+def drawboxes():
+    turtle.setheading(270)
+
+    for _ in range(2):
+        for fill_color in ('blue', 'darkblue'):
+            turtle.fillcolor(fill_color)
+            turtle.begin_fill()
+
+            for _ in range(2):
+                turtle.forward(100)
+                turtle.left(90)
+                turtle.forward(400)
+                turtle.left(90)
+
+            turtle.end_fill()
+
+            turtle.penup()
+            turtle.forward(100)
+            turtle.pendown()
+
+
+screen = Screen()
+
+turtle = Turtle()
+turtle.pencolor('white')
+
+drawboxes()
+
+screen.exitonclick()
